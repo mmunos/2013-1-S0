6937901bdb663c7a3f02a0c4ad87b89f36681f73
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2013-09-25 12:21:37 -0300

    Empty array on params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,8 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    params[:user][:movie_ids] ||= []
+    params[:user][:serial_ids] ||= []
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }

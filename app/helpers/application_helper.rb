f904124e64f333d0e4be21707b8c649514581ace
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2013-10-24 16:59:40 -0300

    Add/Remove action as text for movies/serials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,20 @@ module ApplicationHelper
 		end
 	    link_to link_text, link_path, class: class_name
 	end
+
+		def add_remove_link(show)
+			if show.is_a?(Serial)
+				if current_user && current_user.serials.include?(show)
+					link_to "Remove from My Series", remove_serial_path, class: "remove-show-button"
+				else
+					link_to "Add to My Series", add_serial_path, class: "add-show-button"
+				end
+			elsif show.is_a?(Movie)
+				if current_user && current_user.movies.include?(show)
+					link_to "Remove from My Movies", remove_movie_path, class: "remove-show-button"
+				else
+					link_to "Add to My Movies", add_movie_path, class: "add-show-button"
+				end
+			end
+	end
 end

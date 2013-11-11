7a827272cca2f7c5c9726f687a1ae1f5a1a8ed50
slakat <slakat@gmail.com>
2013-11-10 23:50:03 -0300

    paperclip test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable , polymorphic: true
 
+  has_attached_file :photo, :styles => { :small => "150x150>" },
+                  :url  => "/assets/posts/:id/:style/:basename.:extension",
+                  :path => ":rails_root/public/assets/posts/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+
 end

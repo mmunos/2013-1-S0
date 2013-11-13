c1a1264b069564618120e65dea26be8575fe3c75
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2013-11-13 13:13:08 -0300

    Post form refactored

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -61,17 +61,49 @@ $(document).on("click",".open-post-form", function(e) {
 		}else {
 			$(this).parent('.center-warning').fadeOut();
 		}
-		$(".post-form-container").slideDown(400, function() {
+		$(".post-form-container").slideDown(400, function() {
 			$(".post-form-container textarea").focus();
 		});
 });
 
+$(document).on("change", ".post-form-type-option input", function(e) {
+	var type = $(this).val();
+
+	$(".post-form-type-option label").addClass("small");
+	$(".post-form-type-option").find(".selected").removeClass('selected');
+	$("label[for="+type+"-option]").addClass("selected");
+
+	$(".post-form-type").slideUp(400, function() {
+		if (type == "link") {
+			var file_input = $("#post-form-file-input");
+			file_input.wrap("<form>").parent().get(0).reset();
+			$(".post-form-image-filename").html("You have not selected any image.")
+			file_input.unwrap();
+		} else if (type = "image") {
+			$(".post-form-link input").val("");
+		}
+	});
+	$(".post-form-"+type+", #post-caption").fadeIn();
+});
+
+$(document).on("change", "#post-form-file-input", function(e) {
+	var filename_array = $(this).val().split("\\");
+	var filename = filename_array[filename_array.length-1];
+	$(".post-form-image-filename").html(filename);
+});
 
 $(document).on("click",".close-post-form", function(e) {
 		e.preventDefault();
 		$(".fade-open-post, #posts p.center-warning").fadeIn();
 		$(".post-form-container").slideUp(400, function(){
 			$(".post-form").closest("form")[0].reset();
+			$(".post-form-type-option").find(".selected").removeClass('selected');
+			$(".post-form-type, #post-caption").hide();
+			$(".post-form-type-option label.small").removeClass("small");
+			var file_input = $("#post-form-file-input");
+			file_input.wrap("<form>").parent().get(0).reset();
+			$(".post-form-image-filename").html("You have not selected any image.")
+			file_input.unwrap();
 		});
 });
 
@@ -83,10 +115,8 @@ $(document).bind("ajaxSend", function(){
 
 function showganizer(){
 	$(".flash-bar").delay(3000).fadeOut();
-	$(".review-form-container").hide();
-	$(".post-form-container").hide();
+	$(".review-form-container, .review-form-charleft, .post-form-container, .post-form-image, .post-form-link, #post-caption").hide();
 
-	$(".review-form-charleft").hide()
 
 	$("#review_content").keyup(function(e) {
 		var charleft = 1000-$(this).val().length;
